711a688b398927d192ce81a5f3f7e56632d305a7
SAMAR2101 <samarmiled864@gmail.com>
2024-12-26 11:15:52 +0100

    first commit

---
+++ src/players.js
@@ -0,0 +1,27 @@
+const players = [
+    {
+        name: "Kylian Mbappé",
+        team: "Real Mardrid",
+        nationality: "French",
+        jerseyNumber:10,
+        age: 26,
+        imageUrl:"/images/img.jpeg"
+    },
+    {
+        name: "Cristiano Renaldo",
+        team: "Al Nasser",
+        nationality: "Portogal",
+        jerseyNumber:7,
+        age: 39,
+        imageUrl:"/images/licensed-image.webp"
+    },
+    {
+        name: "NEYMAR jr",
+        team: "Paris Saaint-Germain",
+        nationality: "France",
+        jerseyNumber:7,
+        age: 25,
+        imageUrl:"/images/images.jpeg"
+    }
+];
+export default players;
